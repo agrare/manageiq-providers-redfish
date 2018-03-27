0e5d92060f6e995f0c97259769a1fcdf1a1ea0f7
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-03-27 14:25:06 +0200

    Implement asset details collector
    New functionality from this commit stores information that is not
    strictly related to the physical server to asset_details table that
    servers as a source of data for summary view in ManageIQ UI.
    
    Please take note that asset details mapping is fairly raw right now,
    since mapping tree-like Redfish structures into flat array of XClarity
    properties is not trivial and will probably need some tweaking based
    on real data that we will get from test bed before we get it right.

--- app/models/manageiq/providers/redfish/inventory/collector/physical_infra_manager.rb
+++ app/models/manageiq/providers/redfish/inventory/collector/physical_infra_manager.rb
@@ -3,5 +3,23 @@ module ManageIQ::Providers::Redfish
     def physical_servers
       rf_client.Systems.Members.collect(&:raw)
     end
+
+    def physical_server_details
+      rf_client.Systems.Members.collect { |s| get_server_location(s) }
+    end
+
+    private
+
+    def get_server_location(server)
+      chassis = [server.Links.Chassis[0]]
+      while chassis.last.Links.respond_to?("ContainedBy")
+        chassis.push(chassis.last.Links.ContainedBy)
+      end
+      # TODO(tadeboro): Location can also be string in old version of schema
+      loc = { :server_id => server["@odata.id"] }
+      chassis.reduce(loc) do |acc, c|
+        acc.merge!(c.respond_to?(:Location) ? c.Location.raw : {})
+      end
+    end
   end
 end
